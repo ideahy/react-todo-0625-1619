30b5337dc808b305fa41bcd59aa355594e01113e
hideakiyamamoto <61575280+ideahy@users.noreply.github.com>
2021-06-26 16:15:23 +0900

    complete todolist

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,9 @@ export const App = () => {
   //入力値ステート(onChangeで変更を検知 → 初期値を変更)
   const [todoText, setTodoText] = useState("");
   //未完了ステート
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "アイウエオ",
-    "かきくけrこ"
-  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
   //完了ステート
-  const [completeTodos, setCompleteTodos] = useState(["うアウアウあ"]);
+  const [completeTodos, setCompleteTodos] = useState([]);
   //変更を検知(引数eventの値を入力値ステートに反映)
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   //入力値を未完了ステートに反映(ステートに入力値を追加 → mapで表示)
@@ -35,6 +32,16 @@ export const App = () => {
     //完了ステートへ追加
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setCompleteTodos(newCompleteTodos);
+  };
+  //完了ステートから削除＋未完了ステートへ追加
+  const onclickBack = (index) => {
+    //完了ステートから削除
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+    //未完了ステートへ追加
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
   };
 
   return (
@@ -68,7 +75,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onclickBack(index)}>戻す</button>
               </div>
             );
           })}
